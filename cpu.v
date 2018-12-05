e4526039e32ad0d3195e52b0e9cae2edba2af7b6
Wocanilo <wocat@Alien>
2018-12-05 15:40:04 +0100

    Terminado

--- cpu.v
+++ cpu.v
@@ -5,9 +5,13 @@ module cpu(
 );
 
 reg[7:0] registers[0:15];
-reg [7:0]pc; //Program counter
+reg [15:0]pc; //Program counter
 reg carry; // Separamos el contador de carry de los registros.
 reg skip; // Permite saltar la siguiente instrucción
+reg [15:0]stack[0:16]; // Stack de 16bits y 16 niveles.
+reg [3:0]stack_pointer; // Profundidad actual del stack.
+reg [15:0]delay_timer;
+reg [15:0] I;
 
 //Instrucciones
 reg [3:0]op; 
@@ -17,16 +21,16 @@ reg [3:0]arg_z;
 
 
 //Memoria
-reg [7:0] ROM [3327:0]; // Declaración de memoria de 3328 Bytes
+reg [7:0] ROM [0:4096]; // Declaración de memoria de 4096 Bytes
 
 integer i;
 
 initial begin
-	for (i=0; i < 3328; i = i + 1) begin
+	for (i=0; i < 4096; i = i + 1) begin
 		ROM[i] = 0;
 	end
 	$display("Loading rom.");
-	$readmemb("Chip8Test.b", ROM); // Cargamos la rom en memoria
+	$readmemb("Factorial.b", ROM, 512); // Cargamos la rom en memoria
 end
 
 //Fin Memoria
@@ -35,13 +39,19 @@ initial begin
 	for (i=0; i < 15; i = i + 1) begin
 		registers[i] = 0;
 	end
-	pc = 0;
+	for (i=0; i < 16; i = i + 1) begin
+		stack[i] = 0;
+	end
+	pc = 512;
 	op = 0;
 	arg_x = 0;
 	arg_y = 0;
 	arg_z = 0;
 	carry = 0;
 	skip = 0;
+	stack_pointer = 0;
+	delay_timer = 0;
+	I = 0;
 end
 
 reg [15:0]instruction_debug = 0;
@@ -52,18 +62,25 @@ always @(rst) begin
 	for (i=0; i < 15; i = i + 1) begin
 		registers[i] = 0;
 	end
-	pc = 0;
+	for (i=0; i < 16; i = i + 1) begin
+		stack[i] = 0;
+	end
+	pc = 512;
 	op = 0;
 	arg_x = 0;
 	arg_y = 0;
 	arg_z = 0;
 	carry = 0;
 	skip = 0;
+	stack_pointer = 4'b0;
+	delay_timer = 0;
+	I = 0;
 end
 
 always @(posedge clk) begin
-	$monitor("clk=%b, PC=%hex, INSTRUCTION=%hex, V1=%b, V2=%b, V3=%b, V4=%b, carry=%b, skip=%b", clk, pc, instruction_debug, registers[1], registers[2], registers[3], registers[4], carry, skip);
+	$monitor("clk=%b, PC=%hex, INSTRUCTION=%hex, V0=%b, V1=%b, V2=%b, V3=%b, V4=%b, carry=%b, skip=%b, stack_pointer=%b, stack_0=%h, I=%h", clk, pc, instruction_debug, registers[0], registers[1], registers[2], registers[3], registers[4], carry, skip, stack_pointer, stack[0], I);
 	instruction_debug <= {op, arg_x, arg_y, arg_z};
+	if (delay_timer > 0) delay_timer <= delay_timer - 1;
 	// Aumentamos el PC. Cada instruccion son 2 bytes
 	pc <= pc + 2;
 
@@ -80,8 +97,21 @@ always @(posedge clk) begin
 	if(skip == 0)
 		begin
 			case(op)
+				4'd0: // The interpreter sets the program counter to the address at the top of the stack, then subtracts 1 from the stack pointer.
+					if (arg_z == 4'hE) begin
+						pc <= stack[stack_pointer - 1];
+						stack_pointer <= stack_pointer - 1;
+					end
 				4'd1: pc <= {arg_x, arg_y, arg_z}; // 1nnn - Jump to location nnn.
+				4'd2: // The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
+					begin
+						stack[stack_pointer] <= pc;
+						pc <= {arg_x, arg_y, arg_z};
+						stack_pointer <= stack_pointer + 1;
+					end
 				4'd3: if (registers[arg_x] == {arg_y, arg_z}) skip <= 1;  // The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
+				4'd4: if (registers[arg_x] != {arg_y, arg_z}) skip <= 1; //  The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
+				4'd5: if (registers[arg_x] == registers[arg_y]) skip <= 1; //  The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
 				4'd6: registers[arg_x] <= {arg_y, arg_z}; // The interpreter puts the value kk into register Vx.
 				4'd7: registers[arg_x] <= registers[arg_x] + {arg_y, arg_z}; // Adds the value kk to the value of register Vx, then stores the result in Vx. 
 				4'd8: 
@@ -104,11 +134,19 @@ always @(posedge clk) begin
 								registers[arg_x] = registers[arg_x] << 1;
 							end
 					endcase
+				4'd9: if(registers[arg_x] != registers[arg_y]) skip <= 1; //  The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
+				4'hA: I <= {arg_x, arg_y, arg_z};
+				4'hB: pc <= {arg_x, arg_y, arg_z} + registers[0]; //  The program counter is set to nnn plus the value of V0.
 				4'hC: 
 					begin // Implementar número aleatorio
 						registers[arg_x] = 'b00001001 & {arg_y, arg_z};
 					end
-
+				4'hF:
+					case(arg_z)
+						4'd5: delay_timer <= registers[arg_x]; // DT is set equal to the value of Vx.
+						4'd7: registers[arg_x] <= delay_timer; // The value of DT is placed into Vx.
+						4'hE: I <= I + registers[arg_x];
+					endcase	
 			endcase
 		end
 		else skip <= 0;
